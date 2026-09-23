59d7c3e07c5bbb249c5af8e237f5790108b6185b
Argil <argildevs@gmail.com>
2026-09-23 01:20:53 +0100

    v1.0.0

---
+++ README.md
@@ -0,0 +1,2 @@
+# Classic Negatives
+Classic Negatives is a mod that adds back the negative shader from versions before [v0.9.0q-DEMO](https://balatrowiki.org/w/Update_0.9.0q-DEMO).
---
+++ main.lua
@@ -0,0 +1,13 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Classic Negatives
+--- MOD_ID: CNeg
+--- MOD_AUTHOR: [ArgilRants]
+--- MOD_DESCRIPTION: Pre 0.9.0q-DEMO negative shader
+--- PREFIX: cneg
+--- VERSION: 1.0.0
+ 
+SMODS.Shader{
+    key = 'negative',
+    path = 'negative.fs',
+    prefix_config = { key = false },
+}
